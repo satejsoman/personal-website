8fa77bcce468391d7fc282e8472886f3432a4928
satej soman <satej.soman@gmail.com>
2020-11-17 07:47:01 -0600

    fix google analutics

--- website/src/App.js
+++ website/src/App.js
@@ -7,25 +7,18 @@ import Art from './pages/Art.js';
 import Research from './pages/Research.js';
 import Covid from './pages/covid/Covid.js'
 import Slums from './pages/slums/Slums.js'
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
+import Analytics from 'react-router-ga';
 
 const googleAnalyticsId = "G-DV45FC0XBC";
-ReactGA.initialize(googleAnalyticsId);
 
-const history = createBrowserHistory();
-history.listen(location => {
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
-const App = (_) =>
-<Router history = {history}>
-    <Route exact path='/'               component={Home}/>
-    <Route exact path='/art'            component={Art}/>
-    <Route exact path='/research'       component={Research}/>
-    <Route exact path='/research/slums' component={Slums}/>
-    <Route exact path='/research/covid' component={Covid}/>
-</Router>
-
+const App = () => { 
+    return <Router><Analytics id={googleAnalyticsId}>
+        <Route exact path='/'               component={Home}/>
+        <Route exact path='/art'            component={Art}/>
+        <Route exact path='/research'       component={Research}/>
+        <Route exact path='/research/slums' component={Slums}/>
+        <Route exact path='/research/covid' component={Covid}/>
+    </Analytics></Router>
+} 
 export default App;
