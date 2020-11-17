e9e49171c4f69a345b9a25ba642beba50b1a5fd2
satej soman <satej.soman@gmail.com>
2020-11-17 07:31:30 -0600

    google analytics

--- website/src/App.js
+++ website/src/App.js
@@ -7,9 +7,20 @@ import Art from './pages/Art.js';
 import Research from './pages/Research.js';
 import Covid from './pages/covid/Covid.js'
 import Slums from './pages/slums/Slums.js'
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+const googleAnalyticsId = "G-DV45FC0XBC";
+ReactGA.initialize(googleAnalyticsId);
+
+const history = createBrowserHistory();
+history.listen(location => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const App = (_) =>
-<Router>
+<Router history = {history}>
     <Route exact path='/'               component={Home}/>
     <Route exact path='/art'            component={Art}/>
     <Route exact path='/research'       component={Research}/>
