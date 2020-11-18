ea732db95886a0d20eddb20beb8aa1d6160582e1
satej soman <satej.soman@gmail.com>
2020-11-18 01:37:40 -0600

    simpler GA impl

--- website/src/App.js
+++ website/src/App.js
@@ -7,18 +7,15 @@ import Art from './pages/Art.js';
 import Research from './pages/Research.js';
 import Covid from './pages/covid/Covid.js'
 import Slums from './pages/slums/Slums.js'
-import Analytics from 'react-router-ga';
-
-const googleAnalyticsId = "G-DV45FC0XBC";
 
 
 const App = () => { 
-    return <Router><Analytics id={googleAnalyticsId}>
+    return <Router>
         <Route exact path='/'               component={Home}/>
         <Route exact path='/art'            component={Art}/>
         <Route exact path='/research'       component={Research}/>
         <Route exact path='/research/slums' component={Slums}/>
         <Route exact path='/research/covid' component={Covid}/>
-    </Analytics></Router>
+    </Router>
 } 
 export default App;
