6ef1b69f54b56d736303af9231b86cf61076054a
satej soman <satej.soman@gmail.com>
2020-12-22 00:11:50 -0600

    just update

--- website/src/App.js
+++ website/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 
-import { HashRouter as Router, Redirect, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Art from './pages/Art.js';
 import Research from './pages/Research.js';
 import Covid from './pages/covid/Covid.js'
 import Slums from './pages/slums/Slums.js'
-// import CV from "./assets/CV.pdf"
+import CV from "./assets/CV.pdf"
 
 const App = () => { 
     return <Router>
@@ -16,10 +16,7 @@ const App = () => {
         <Route exact path='/research'       component={Research}/>
         <Route exact path='/research/slums' component={Slums}/>
         <Route exact path='/research/covid' component={Covid}/>
-        <Route exact path='/CV'>
-            <Redirect to={process.env.PUBLIC_URL + "/CV.pdf"}/>
-        </Route>
-        {/* <Route exact path='/CV.pdf'         component={CV}/> */}
+        <Route exact path='/CV'><a href={CV}/></Route>
     </Router>
 } 
 export default App;
